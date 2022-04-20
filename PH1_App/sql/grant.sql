2801549c4aa3d40eb8e038310d4bbe62bed75f0e
TungPham24 <tung38085@gmail.com>
2022-04-20 17:42:35 +0700

    file phan quyen admin

---
+++ PH1_App/sql/grant.sql
@@ -0,0 +1,23 @@
+grant all privileges on SYS.HSBA to "900004" WITH GRANT OPTION;
+grant all privileges on SYS.HSBA_DV to "900004" WITH GRANT OPTION;
+grant all privileges on SYS.BENHNHAN to "900004" WITH GRANT OPTION;
+grant all privileges on SYS.NHANVIEN to "900004" WITH GRANT OPTION;
+grant all privileges on SYS.CSYT to "900004" WITH GRANT OPTION;
+
+grant all privileges on SYS.HSBA to "900001" WITH GRANT OPTION;
+grant all privileges on SYS.HSBA_DV to "900001" WITH GRANT OPTION;
+grant all privileges on SYS.BENHNHAN to "900001" WITH GRANT OPTION;
+grant all privileges on SYS.NHANVIEN to "900001" WITH GRANT OPTION;
+grant all privileges on SYS.CSYT to "900001" WITH GRANT OPTION;
+
+grant all privileges on SYS.HSBA to "900002" WITH GRANT OPTION;
+grant all privileges on SYS.HSBA_DV to "900002" WITH GRANT OPTION;
+grant all privileges on SYS.BENHNHAN to "900002" WITH GRANT OPTION;
+grant all privileges on SYS.NHANVIEN to "900002" WITH GRANT OPTION;
+grant all privileges on SYS.CSYT to "900002" WITH GRANT OPTION;
+
+grant all privileges on SYS.HSBA to "900003" WITH GRANT OPTION;
+grant all privileges on SYS.HSBA_DV to "900003" WITH GRANT OPTION;
+grant all privileges on SYS.BENHNHAN to "900003" WITH GRANT OPTION;
+grant all privileges on SYS.NHANVIEN to "900003" WITH GRANT OPTION;
+grant all privileges on SYS.CSYT to "900003" WITH GRANT OPTION;
