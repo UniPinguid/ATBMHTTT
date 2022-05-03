5a321dc002bbd31c9d90be492794324147e31dca
UniPinguid <bao.phat.50@gmail.com>
2022-05-03 20:27:35 +0700

    Update ATBMCQ-06_19120114_Oracle_VPD1.sql

--- ATBMCQ-06_19120114_Oracle_VPD1.sql
+++ ATBMCQ-06_19120114_Oracle_VPD1.sql
@@ -4,3 +4,18 @@
 --   19120172 - Nguyễn Sơn Bão
 --   19120261 - Nguyễn Hữu Khôi
 --   19120423 - Phạm Sơn Tùng
+
+CREATE TABLE EMPHOLIDAY
+(
+    EmpNo NUMBER(5),
+    EName VARCHAR2(60),
+    Holiday DATE
+);
+/
+
+INSERT INTO EMPHOLIDAY(EmpNo, EName, Holiday) VALUES (1, 'Hann', TO_DATE('2/1/2019', 'dd/mm/yyyy'));
+/
+INSERT INTO EMPHOLIDAY(EmpNo, EName, Holiday) VALUES (2, 'Annu', TO_DATE('12/5/2019', 'dd/mm/yyyy'));
+/
+INSERT INTO EMPHOLIDAY(EmpNo, EName, Holiday) VALUES (3, 'Theota', TO_DATE('26/8/2018', 'dd/mm/yyyy'));
+/
